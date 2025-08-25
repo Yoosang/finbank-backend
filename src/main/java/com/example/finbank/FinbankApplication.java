1274a0bebf32c3a596f3448044c7e80c7e5b5a23
usang <usang6325@gmail.com>
2025-08-26 00:08:04 +0900

    project init

---
+++ src/main/java/com/example/finbank/FinbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.finbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinbankApplication.class, args);
+	}
+
+}
